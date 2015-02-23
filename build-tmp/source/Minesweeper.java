50a4b609ce439c76d93574032f41f39c9b8fd9bb
gdare <gregory.dare27@gmail.com>
2015-02-23 11:16:14 -0800

    MineSweeper

--- build-tmp/source/Minesweeper.java
+++ build-tmp/source/Minesweeper.java
@@ -1,4 +1,22 @@
-import de.bezier.guido.*;
+import processing.core.*; 
+import processing.data.*; 
+import processing.event.*; 
+import processing.opengl.*; 
+
+import de.bezier.guido.*; 
+
+import java.util.HashMap; 
+import java.util.ArrayList; 
+import java.io.File; 
+import java.io.BufferedReader; 
+import java.io.PrintWriter; 
+import java.io.InputStream; 
+import java.io.OutputStream; 
+import java.io.IOException; 
+
+public class Minesweeper extends PApplet {
+
+
 //Declare and initialize NUM_ROWS and NUM_COLS = 20
 private MSButton[][] buttons; //2d array of minesweeper buttons
 private ArrayList <MSButton> bombs; //ArrayList of just the minesweeper buttons that are mined
@@ -14,7 +32,7 @@ String[] winMessage = {
   "Y", "O", "U", " ", "W", "I", "N", "!"
 };
 
-void setup ()
+public void setup ()
 {
   size(400, 400);
   textAlign(CENTER, CENTER);
@@ -170,7 +188,7 @@ public class MSButton
       if (!isWon())
         fill(0);
       else
-        fill(#FFF527);
+        fill(0xffFFF527);
     else if ( clicked && bombs.contains(this) ) 
       fill(255, 0, 0);
     else if (clicked)
@@ -203,5 +221,14 @@ public class MSButton
             numBombs++;
 
     return numBombs;
+  }
+}
+  static public void main(String[] passedArgs) {
+    String[] appletArgs = new String[] { "Minesweeper" };
+    if (passedArgs != null) {
+      PApplet.main(concat(appletArgs, passedArgs));
+    } else {
+      PApplet.main(appletArgs);
+    }
   }
 }
